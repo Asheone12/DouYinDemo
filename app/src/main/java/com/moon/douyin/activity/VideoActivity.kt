45193d586a657f4d56a1650086383f9526d25192
xujianglong <jianglong.xu@tiho-mobi.com>
2025-05-13 09:44:36 +0800

    无限视频

--- app/src/main/java/com/moon/douyin/activity/VideoActivity.kt
+++ app/src/main/java/com/moon/douyin/activity/VideoActivity.kt
@@ -1,11 +1,20 @@
 package com.moon.douyin.activity
 
+import androidx.viewpager2.widget.ViewPager2
 import com.moon.douyin.adapter.ItemViewPagerAdapter
 import com.moon.douyin.databinding.ActivityVideoBinding
 
 class VideoActivity : BaseActivity<ActivityVideoBinding>() {
     private lateinit var mPagerAdapter: ItemViewPagerAdapter
     private val urlList = mutableListOf(
+        "https://sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/flv/xgplayer-demo-360p.flv",
+        "https://stream7.iqilu.com/10339/upload_transcode/202002/18/20200218114723HDu3hhxqIT.mp4",
+        "https://stream7.iqilu.com/10339/upload_transcode/202002/18/20200218093206z8V1JuPlpe.mp4",
+        "https://stream7.iqilu.com/10339/article/202002/18/2fca1c77730e54c7b500573c2437003f.mp4",
+        "https://stream7.iqilu.com/10339/upload_transcode/202002/18/20200218025702PSiVKDB5ap.mp4",
+        "https://stream7.iqilu.com/10339/upload_transcode/202002/18/202002181038474liyNnnSzz.mp4"
+    )
+    private val addedUrlList =  mutableListOf(
         "https://sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/flv/xgplayer-demo-360p.flv",
         "https://stream7.iqilu.com/10339/upload_transcode/202002/18/20200218114723HDu3hhxqIT.mp4",
         "https://stream7.iqilu.com/10339/upload_transcode/202002/18/20200218093206z8V1JuPlpe.mp4",
@@ -22,5 +31,15 @@ class VideoActivity : BaseActivity<ActivityVideoBinding>() {
         super.initView()
         mPagerAdapter = ItemViewPagerAdapter(this, urlList)
         viewBinding.viewPager.adapter = mPagerAdapter
+        viewBinding.viewPager.registerOnPageChangeCallback(object :
+            ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                if (position == urlList.size - 1) {
+                    urlList.addAll(addedUrlList)
+                    mPagerAdapter.notifyDataSetChanged()
+                }
+            }
+        })
     }
 }
